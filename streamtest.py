33e132c83ca5d9b18b58b3802f95c2c60e472977
Alan <a.hengstmannn@gmail.com>
2022-02-06 15:52:42 -0300

    Pie Chart + Explanation

--- streamtest.py
+++ streamtest.py
@@ -3,12 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from app import predict_wine
 from PIL import Image
+import plotly.express as px
+
 
 class StreamlitApp:
 
 
     def __init__(self):
         self.title = st.title('Wine Prediction Project')
+        with st.expander("See explanation"):
+            st.write("""
+         The objective of this project is to predict the quality of a wine from its physicochemical properties. 
+         Data were extracted from kaggle for study purposes. 
+         There are several possibilities of application in development and quality control in the wine making process from this solution.
+     """)
         self.sidetitle = st.sidebar.text('Properties')
         self.tipo = st.sidebar.selectbox('Type',('White Wine','Red Wine'))
         self.df = pd.read_csv(r'winequalityN.csv')
@@ -27,7 +35,8 @@ class StreamlitApp:
             self.wine = predict_wine(mapper[self.tipo], self.prop[0], self.prop[1],self.prop[2],self.prop[3],self.prop[4],self.prop[5],self.prop[6],self.prop[7],self.prop[8],self.prop[9],self.prop[10])
             st.header(f"Wine Quality: {self.wine[0]}")
             st.markdown('Probability of Class')
-            st.bar_chart(self.wine[1])
+            fig = px.pie(self.wine[1], values=self.wine[1][0], names=['Bad Wine','Good Wine'], color_discrete_sequence=px.colors.sequential.Burgyl)
+            st.plotly_chart(fig)
         else:
             st.write('')
 
