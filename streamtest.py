5ed8b299c4a46f4683ecfa89c14d5dcbe362439e
Alan <a.hengstmannn@gmail.com>
2022-02-06 14:24:11 -0300

    Update Web Service

--- streamtest.py
+++ streamtest.py
@@ -1,35 +1,38 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import requests
 from app import predict_wine
+from PIL import Image
 
 class StreamlitApp:
 
 
     def __init__(self):
         self.title = st.title('Wine Prediction Project')
-
-        df = pd.read_csv(r'C:\Users\aheng\OneDrive\Data Science Knowledge\Projetinhos\Wine-Prediction\winequalityN.csv')
-        cols = list(df.columns)
         self.sidetitle = st.sidebar.text('Properties')
         self.tipo = st.sidebar.selectbox('Type',('White Wine','Red Wine'))
-        objects = {}
-        for col in cols[1:-1]:
-            objects[col]=st.sidebar.slider(col,float(df[col].min()),float(df[col].max()),float(df[col].mean()))
+        self.df = pd.read_csv(r'winequalityN.csv')
 
-        listobjs = list(objects.values())
+        self.cols = list(self.df.columns)
+        self.properties = {}
+        for col in self.cols[1:-1]:
+            self.properties[col]=st.sidebar.slider(col,float(self.df[col].min()),float(self.df[col].max()),float(self.df[col].mean()))
+
+        self.prop = list(self.properties.values())
         mapper = {'White Wine':0,'Red Wine':1}
-        
+
+
         if st.button('Wine Predict'):
 
-            Wine = predict_wine(mapper[self.tipo], listobjs[0], listobjs[1],listobjs[2],listobjs[3],listobjs[4],listobjs[5],listobjs[6],listobjs[7],listobjs[8],listobjs[9],listobjs[10])
-            st.write(f"Wine Quality: {Wine[0]}")
-            st.markdown('Probability of Class')
-            st.bar_chart(Wine[1])
+            self.wine = predict_wine(mapper[self.tipo], self.prop[0], self.prop[1],self.prop[2],self.prop[3],self.prop[4],self.prop[5],self.prop[6],self.prop[7],self.prop[8],self.prop[9],self.prop[10])
+            st.write(f"Wine Quality: {self.wine[0]}")
+            st.title('Probability of Class')
+            st.bar_chart(self.wine[1])
         else:
             st.write('')
 
+        self.image = Image.open('WineTypes.jpg')
+        st.image(self.image, caption='Wine Types', use_column_width='auto')
 
 
 
